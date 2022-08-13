15c4a55f80d63b8d484b50d2980e83dfcc2c2976
Harshita <harshitalakhchaura@gmail.com>
2022-08-13 11:57:39 +0530

    Conflicts Resolved

--- src/components/CoinsTable.jsx
+++ src/components/CoinsTable.jsx
@@ -16,11 +16,6 @@ function NumberWithCommas(x) {
 
 export { NumberWithCommas }
 
-function NumberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-export { NumberWithCommas }
 
 function CoinsTable() {
 
